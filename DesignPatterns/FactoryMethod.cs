672fa58fe06e870a082fe783b37189442c2d6fc7
Alexander Régio <alexanderregio@hotmail.com>
2022-09-09 13:39:22 -0300

    FactoryMethod

---
+++ DesignPatterns/FactoryMethod.cs
@@ -0,0 +1,150 @@
+﻿namespace DesignPatterns;
+
+public static class FactoryMethod
+{
+    public static void CallExample()
+    {
+        var impostos = new List<ImpostoCreator>
+        {
+            new IptuEdificadoResidencialCreator(),
+            new IptuEdificadoNaoResidencialCreator(),
+            new IptuTerrenoCreator()
+        };
+
+        impostos.ForEach
+        (i => Console.WriteLine($"O valor do {i} sobre {153_302.00m:C} é de {i.Calcular(153_302.00m):C}"));
+    }
+}
+
+/// <summary>
+/// Interface que possui o contrato ValorAplicado com responsabilidade de retornar o valor do imposto aplicado
+/// </summary>
+public interface IImposto { public decimal Calcular(decimal valor); }
+
+/// <summary>
+/// Classe concreta que herda da interface Imposto
+/// Responsável por calcular o IPTU conforme o valor do venal do imóvel
+/// Obs.: esta classe se apoia somente no cálculo do IPTU de imovéis edificados residenciais
+/// </summary>
+public class IptuEdificadoResidencial : IImposto
+{
+    public decimal Calcular(decimal valor)
+    {
+        return valor switch
+        {
+            <= decimal.Zero => decimal.Zero,
+            > 0.01m and <= 153_302 => valor * 0.006m,
+            > 153_302 and <= 383_258 => valor * 0.007m,
+            > 383_258 and <= 670_705 => valor * 0.0075m,
+            > 670_705 and <= 1_149_786 => valor * 0.0080m,
+            > 1_149_786 and <= 1_533_050 => valor * 0.0085m,
+            > 1_533_050 and <= 1_916_313 => valor * 0.0090m,
+            _ => valor * 0.010m
+        };
+    }
+}
+
+/// <summary>
+/// Classe concreta que herda da interface Imposto
+/// Responsável por calcular o IPTU conforme o valor do venal do imóvel
+/// Obs.: esta classe se apoia somente no cálculo do IPTU de imovéis edificados NÃO residenciais
+/// </summary>
+public class IptuEdificadoNaoResidencial : IImposto
+{
+    public decimal Calcular(decimal valor)
+    {
+        return valor switch
+        {
+            <= decimal.Zero => decimal.Zero,
+            > 0.01m and <= 57_483 => valor * 0.012m,
+            > 57_483 and <= 191_626 => valor * 0.013m,
+            > 191_626 and <= 958_152 => valor * 0.014m,
+            > 958_152 and <= 1_916_313 => valor * 0.015m,
+            _ => valor * 0.016m
+        };
+    }
+}
+
+/// <summary>
+/// Classe concreta que herda da interface Imposto
+/// Responsável por calcular o IPTU conforme o valor do venal do imóvel
+/// Obs.: esta classe se apoia somente no cálculo do IPTU de terrenos e lotes NÃO edificados
+/// </summary>
+public class IptuTerreno : IImposto
+{
+    public decimal Calcular(decimal valor)
+    {
+        return valor switch
+        {
+            <= decimal.Zero => decimal.Zero,
+            > 0.01m and <= 76_646 => valor * 0.01m,
+            > 76_646 and <= 574_890 => valor * 0.016m,
+            > 574_890 and <= 1_149_786 => valor * 0.02m,
+            > 1_149_786 and <= 1_916_313 => valor * 0.025m,
+            _ => valor * 0.03m
+        };
+    }
+}
+
+// FACTORY METHOD
+
+/// <summary>
+/// Classe abstrata para criar as instâncias concretas
+/// </summary>
+public abstract class ImpostoCreator : IImposto
+{
+    public abstract IImposto Create();
+
+    public decimal Calcular(decimal valor)
+    {
+        return Create().Calcular(valor);
+    }
+}
+
+/// <summary>
+/// Classe concreta para cálculo do IPTU de imovéis edificados residenciais
+/// </summary>
+public sealed class IptuEdificadoResidencialCreator : ImpostoCreator
+{
+    public override IImposto Create()
+    {
+        return new IptuEdificadoResidencial();
+    }
+
+    public override string ToString()
+    {
+        return "IPTU para imovéis edificados residenciais";
+    }
+}
+
+/// <summary>
+/// Classe concreta para cálculo do IPTU de imovéis edificados NÃO residenciais
+/// </summary>
+public sealed class IptuEdificadoNaoResidencialCreator : ImpostoCreator
+{
+    public override IImposto Create()
+    {
+        return new IptuEdificadoNaoResidencial();
+    }
+
+    public override string ToString()
+    {
+        return "IPTU para imovéis edificados não residenciais";
+    }
+}
+
+/// <summary>
+/// Classe concreta para cálculo do IPTU de terrenos e lotes NÃO edificados
+/// </summary>
+public sealed class IptuTerrenoCreator : ImpostoCreator
+{
+    public override IImposto Create()
+    {
+        return new IptuTerreno();
+    }
+
+    public override string ToString()
+    {
+        return "IPTU para terrenos e lotes não edificados";
+    }
+}
---
+++ DesignPatternsTests/FactoryMethodTests.cs
@@ -0,0 +1,78 @@
+using DesignPatterns;
+
+namespace DesignPatternsTests;
+
+public class FactoryMethodTests
+{
+    [Theory]
+    [ClassData(typeof(Imposto_Calcular))]
+    [Trait(nameof(IImposto.Calcular), "IPTU")]
+    public void Imposto_Iptu(ImpostoCreator imposto, decimal valor, decimal impostoExpected)
+    {
+        var impostoActual = imposto.Calcular(valor);
+
+        Assert.Equal(impostoExpected, impostoActual, 2);
+    }
+
+    private class Imposto_Calcular : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            // Imóveis edificados residenciais
+
+            // retorno zerado
+            yield return new object[] { new IptuEdificadoResidencialCreator(), decimal.Zero, decimal.Zero };
+            yield return new object[] { new IptuEdificadoResidencialCreator(), -13.13m, decimal.Zero };
+
+            // faixa 0,60%
+            yield return new object[] { new IptuEdificadoResidencialCreator(), 76_651.00m, 459.91m };
+            yield return new object[] { new IptuEdificadoResidencialCreator(), 153_302.00m, 919.81m };
+
+            // faixa 0,70%
+            yield return new object[] { new IptuEdificadoResidencialCreator(), 153_302.01m, 1_073.11m };
+            yield return new object[] { new IptuEdificadoResidencialCreator(), 268_280.00m, 1_877.96m };
+            yield return new object[] { new IptuEdificadoResidencialCreator(), 383_258.00, 2_682.81m };
+
+            // faixa máxima 1,00%
+            yield return new object[] { new IptuEdificadoResidencialCreator(), 1_916_313.01m, 19_163.13m };
+
+            // Imóveis edificados não residenciais
+
+            // retorno zerado
+            yield return new object[] { new IptuEdificadoNaoResidencialCreator(), decimal.Zero, decimal.Zero };
+            yield return new object[] { new IptuEdificadoNaoResidencialCreator(), -13.13m, decimal.Zero };
+
+            // faixa 1,20%
+            yield return new object[] { new IptuEdificadoNaoResidencialCreator(), 28_741.50m, 344.90m };
+            yield return new object[] { new IptuEdificadoNaoResidencialCreator(), 57_483.00m, 689.80m };
+
+            // faixa 1,30%
+            yield return new object[] { new IptuEdificadoNaoResidencialCreator(), 57_483.01m, 747.28m };
+            yield return new object[] { new IptuEdificadoNaoResidencialCreator(), 124_554.50m, 1_619.21m };
+            yield return new object[] { new IptuEdificadoNaoResidencialCreator(), 191_626.00m, 2_491.14m };
+
+            // faixa máxima 1,60%
+            yield return new object[] { new IptuEdificadoNaoResidencialCreator(), 1_916_313.01m, 30_661.01m };
+
+            // Imóveis terrenos e lotes não edificados
+
+            // retorno zerado
+            yield return new object[] { new IptuTerrenoCreator(), decimal.Zero, decimal.Zero };
+            yield return new object[] { new IptuTerrenoCreator(), -13.13m, decimal.Zero };
+
+            // faixa 1,00%
+            yield return new object[] { new IptuTerrenoCreator(), 38_323.00m, 383.23m };
+            yield return new object[] { new IptuTerrenoCreator(), 76_646.00m, 766.46m };
+
+            // faixa 1,60%
+            yield return new object[] { new IptuTerrenoCreator(), 76_646.01m, 1_226.34m };
+            yield return new object[] { new IptuTerrenoCreator(), 325_768.00m, 5_212.29m };
+            yield return new object[] { new IptuTerrenoCreator(), 574_890.00m, 9_198.24m };
+
+            // faixa máxima 3,00%
+            yield return new object[] { new IptuTerrenoCreator(), 1_916_313.01m, 57_489.39m };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
